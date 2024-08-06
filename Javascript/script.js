5cb87ae170719d05bac46c8755e9eedee04e8500
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2024-08-06 23:57:21 +0300

    3.Gun

---
+++ Javascript/script.js
@@ -0,0 +1,31 @@
+
+// UYGULAMA 1
+
+// let ogr1_name = "ADA";
+// let ogr1_surname = "Bilge";
+// let ogr1_borndate = 2012;
+// let ogr1_exam_notes = [70, 70, 80];
+
+// let ogr2_name = "Yigit";
+// let ogr2_surname = "Bilge";
+// let ogr2_borndate = 2010;
+// let ogr2_exam_notes = [40, 40, 50];
+
+// let ages1 = 2024 - ogr1_borndate;
+// let ages2 = 2024 - ogr2_borndate;
+
+// UYGULAMA 2
+
+// let meyveler = ["Kiraz", "Karpuz", "kavun", "Erik"];
+// sonuc = meyveler.length;
+// console.log(sonuc);
+
+// console.log(meyveler[0], meyveler[meyveler.length - 1]);
+// console.log(meyveler.includes("kavun"));
+// let add = meyveler.concat("cilek");
+// console.log(add);
+// console.log(meyveler.splice(0, meyveler.length - 2));
+
+
+// UYGULAMA 3
+

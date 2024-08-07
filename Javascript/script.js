c3b312b3343c6965c03c8a04a373461eca17fe14
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2024-08-07 17:06:13 +0300

    3.gun

--- Javascript/script.js
+++ Javascript/script.js
@@ -27,26 +27,72 @@
 
 // UYGULAMA 3
 
-let ogrenci1 = ["Yigit", "Bilge", 2010, [60, 90, 80]];
+// let ogrenci1 = ["Yigit", "Bilge", 2010, [60, 90, 80]];
 
-let ogrenci2 = ["ada", "Bilge", 2012, [70, 80, 85]];
+// let ogrenci2 = ["ada", "Bilge", 2012, [70, 80, 85]];
 
-let ogrenci3 = ["Cinar", "Turan", 2017, [60, 50, 80]];
+// let ogrenci3 = ["Cinar", "Turan", 2017, [60, 50, 80]];
 
-let ogrenciler = [ogrenci1, ogrenci2, ogrenci3];
-// Yaslar
-let hesapla1 = 2025 - ogrenci1[2];
-console.log(hesapla1);
+// let ogrenciler = [ogrenci1, ogrenci2, ogrenci3];
+// // Yaslar
+// let hesapla1 = 2025 - ogrenci1[2];
+// console.log(hesapla1);
 
-let hesapla2 = 2025 - ogrenci2[2];
-console.log(hesapla2);
+// let hesapla2 = 2025 - ogrenci2[2];
+// console.log(hesapla2);
 
-let hesapla3 = 2025 - ogrenci3[2];
-console.log(hesapla3);
+// let hesapla3 = 2025 - ogrenci3[2];
+// console.log(hesapla3);
 
-let yigitnot = (ogrenci1[3][0] + ogrenci1[3][1] + ogrenci1[3][2]) / 3;
-console.log(yigitnot);
-let adanot = (ogrenci2[3][0] + ogrenci2[3][1] + ogrenci2[3][2]) / 3;
-console.log(adanot);
-let cinarnot = (ogrenci3[3][0] + ogrenci3[3][1] + ogrenci3[3][2]) / 3;
-console.log(cinarnot);
+// let yigitnot = (ogrenci1[3][0] + ogrenci1[3][1] + ogrenci1[3][2]) / 3;
+// console.log(yigitnot);
+// let adanot = (ogrenci2[3][0] + ogrenci2[3][1] + ogrenci2[3][2]) / 3;
+// console.log(adanot);
+// let cinarnot = (ogrenci3[3][0] + ogrenci3[3][1] + ogrenci3[3][2]) / 3;
+// console.log(cinarnot);
+
+// Uygulama 4
+
+let siparis_1 = {
+  id: 101,
+  muster_id: 12,
+  tarih: "31.12.2025",
+  odeme_sekli: "kredi karti",
+  kargo_adresi: {
+    mahalle: "Yahya kaptan mah.",
+    ilce: "izmit",
+    sehir: "kocaeli",
+  },
+  urunler: [
+    { urun_id: 5, urun_adi: "iphone 16 pro", urun_url: "#", fiyat: 75000 },
+    { urun_id: 6, urun_adi: "iphone 16 pro max", urun_url: "#", fiyat: 85000 },
+  ],
+};
+
+let siparis_2 = {
+  id: 102,
+  muster_id: 12,
+  tarih: "31.12.2025",
+  odeme_sekli: "kredi karti",
+  kargo_adresi: {
+    mahalle: "Yahya kaptan mah.",
+    ilce: "izmit",
+    sehir: "kocaeli",
+  },
+  urunler: [
+    { urun_id: 5, urun_adi: "iphone 16 pro", urun_url: "#", fiyat: 75000 },
+  ],
+};
+let siparis1_toplam =
+  (siparis_1.urunler[0].fiyat + siparis_1.urunler[1].fiyat) * 1.2;
+let siparis2_toplam = siparis_2.urunler[0].fiyat * 1.2;
+
+console.log(
+  "Kdv siz Fiyati:",
+  siparis_1.urunler[0].fiyat + siparis_1.urunler[1].fiyat,
+  "kdv dahil Toplam fiyat:",
+  siparis1_toplam
+);
+console.log(siparis2_toplam);
+toplam_siparis = siparis1_toplam + siparis2_toplam;
+console.log(toplam_siparis);

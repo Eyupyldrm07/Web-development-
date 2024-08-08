1c1dc09a6714c582acb7f82b84385546abbc8ee5
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2024-08-08 23:08:38 +0300

    4.Gun

---
+++ Javascript/Dom/scrypt.js
@@ -0,0 +1,54 @@
+// Single Elements
+
+// let sonuc;
+
+// sonuc = document.getElementById("title");
+// sonuc = document.getElementById("title").id;
+// sonuc = document.getElementById("title").classList;
+
+// document.getElementById("title").style.fontSize = "30px";
+// document.getElementById("title").style.color = "blue";
+// document.getElementById("title").style.backgroundColor = "yellow";
+// document.getElementById("title").innerText = "My first Todo App";
+// document.getElementById("title").innerHTML = "<h1>My first Todo App</h1>";
+
+// sonuc = document.querySelector("#title");
+// sonuc = document.querySelector(".card-header");
+// sonuc = document.querySelector("div");
+
+// sonuc = document.querySelector("li");
+// sonuc = document.querySelector("li:first-child");
+// sonuc = document.querySelector("li:last-child");
+// sonuc = document.querySelector("li:nth-child(2");
+
+// console.log(sonuc);
+
+// Multiple Elements
+
+// let sonuc;
+
+// sonuc = document.getElementsByClassName("task")[0];
+// sonuc = document.getElementsByClassName("task")[1];
+
+// tasklist = document.getElementsByClassName("task");
+// for (let i = 0; i < tasklist.length; i++) {
+//   console.log(tasklist[i]);
+// }
+// for (let task of tasklist) {
+//   task.style.color = "red";
+// }
+
+// console.log(sonuc);
+
+// Travelsal Elements
+// let sonuc;
+// ul = document.getElementById("task-list");
+
+// sonuc = ul.childeren;
+// sonuc = ul.childeren[0];
+// sonuc = ul.firtsElementChild;
+// sonuc = ul.lastElementChild;
+
+// sonuc = document.getElementById("title").parentElement;
+
+// console.log(sonuc);
